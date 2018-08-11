f911d198644c164d28a82374e3d34a659e487014
minhsphuc12 <phucnm.ec@gmail.com>
2018-08-11 14:25:27 +0700

    fix script summary

--- numerical_summary.R
+++ numerical_summary.R
@@ -1,15 +1,14 @@
 library(tidyverse)
+library(rio)
 
-data_folder = 'C:\\Users\\Nasdap\\Documents\\git\\datatalk\\titanic'
+data_folder = 'C:\\Users\\phucn\\Documents\\git\\datatalk\\titanic'
 
 train = as.tibble(import(paste0(data_folder,'\\train.csv')))
 test = as.tibble(import(paste0(data_folder,'\\test.csv')))
 gender_class = as.tibble(import(paste0(data_folder,'\\gender_submission.csv')))
 
 train
-
 test
-
 gender_class
 
 #test and gender_class have same length. So they probably belong together.
@@ -28,10 +27,10 @@ data_full
 
 # >>>>>>> SOLVE Structure: a simple process to explore a dataset numerically. 
 ## 1. Size (S)
-## 2. Key Observation (O)
+## 2. Observation Key (O)
 ## 3. Length (L)
-## 4. Distribution along Variables (V)
-## 5. Question Data Validity (E)
+## 4. Variable Distribution (V)
+## 5. Questions (E)
 
 # Numerical Summary Structure
 ## 1. Size (S)
@@ -39,7 +38,7 @@ data_full
 str(data_full)
 colnames(data_full)
 
-## 2. Key Observation (O)
+## 2. Observation Key (O)
 ## 2. Second, is there any variable serving as observation key? Quickest way is finding any variable with name contains "ID".
 
 colnames(data_full)[grepl('id', tolower(colnames(data_full)))]
@@ -51,16 +50,16 @@ length(unique(data_full$PassengerId)) == length(data_full$PassengerId)
 
 # our dataset has 1309 observations, is this whole passenger count?
 # https://titanicfacts.net/titanic-passengers/
-# 1,317 – the approximate number of passengers actually on board.
-# 324 – the number of first class passengers on board.
-# 284 – the number of second class passengers.
-# 709 – the number of third class passengers.
-# 107 – the number of children travelling, mainly travelling in third class
+# 1,317  the approximate number of passengers actually on board.
+# 324  the number of first class passengers on board.
+# 284  the number of second class passengers.
+# 709  the number of third class passengers.
+# 107  the number of children travelling, mainly travelling in third class
 
 first_class = data_full %>% filter(Pclass == 1)
 # 323 passengers in dataset, vs. 324 in the source, not bad.
 
-## 4. Distribution along Variables (V)
+## 4. Variables Distribution (V)
 ## 4. Forth, for categorical, check count for each category; for continuous, check distribution parameters.
 summary(data_full)
 
@@ -74,7 +73,7 @@ table(data_full$Cabin)
 # this Cabin variable has a lot of value in raw form, so it need to be recoded somehow to be informative. 
 # This is what called "FEATURE ENGINEERING" later.
 
-## 5. Question Data Validity (E)
+## 5. Question (E)
 ## 5. Last, ae doubtful, as data can be missing / wrong input / cooked / etc.
 table(data_full$Pclass, useNA = 'ifany')
 table(data_full$Sex, useNA = 'ifany')
@@ -92,6 +91,7 @@ table(data_full_age$age_missing, data_full_age$Pclass)
 # Pclass is both categorical and numerical 
 table(data_full_age$age_missing, data_full_age$Embarked)
 # missing age has no clear correlation with departure location, so it has nothing to do with recording system.
+
 cor(data_full_age %>% select(c(age_missing, Fare, Pclass, SibSp, Parch, Survived)), method = "pearson", use = "complete.obs")
 # age missing change increase when Fare decrease, higher class (lower class quality), and no significant correlation with number of relatives and survival chance.
 
